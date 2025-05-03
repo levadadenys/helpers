204476e983449718225417f42e2789725431673f
denyslevada <levada.denys@gmail.com>
2025-05-03 19:22:22 +0300

    feat(typescript/algorithms): small order update

--- typescript/algorithms/index.ts
+++ typescript/algorithms/index.ts
@@ -1,2 +1,4 @@
-export * as searchAlgorithms from './search';
-export * as sortAlgorithms from './sort';
+export * as arraySearchAlgorithms from './array/search';
+export * as arraySortAlgorithms from './array/sort';
+
+export * as graphSearchAlgorithms from './graph/search';
