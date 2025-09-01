209f68f4c1fd2d8c6bec3a4f7f93a84dc2caa359
denyslevada <levada.denys@gmail.com>
2025-09-01 12:35:09 +0300

    feat(typescript/algorithms): add room scheduling algorithm and export greedy algorithms

---
+++ typescript/algorithms/greedyAlgorithms/roomScheduleProblem.ts
@@ -0,0 +1,46 @@
+const roomScheduleOptions = [
+  {lesson: 'art', start: '9:00 AM', end: '10:00 AM'},
+  {lesson: 'english', start: '9:30 AM', end: '10:30 AM'},
+  {lesson: 'math', start: '10:00 AM', end: '11:00 AM'},
+  {lesson: 'computer science', start: '10:30 AM', end: '11:30 AM'},
+  {lesson: 'music', start: '11:00 AM', end: '12:00 PM'},
+]
+
+const parseTimeToNumber = (time: string) => {
+  const [timePart, modifier] = time.split(' ');
+  let [hours, minutes] = timePart.split(':').map(Number);
+  if (modifier === 'PM' && hours !== 12) {
+    hours += 12;
+  }
+  return (hours * 60) + minutes;
+}
+
+const getOptimalRoomSchedule = (sortedLessons: {lesson: string, start: string, end: string}[]) => {
+  const optimalSchedule: {lesson: string, start: string, end: string}[] = [];
+
+  let lastEndTime = 0;
+
+  for(let i = 0; i <= sortedLessons.length - 1; i++) {
+    const lesson = sortedLessons[i];
+    const lessonStartTime = parseTimeToNumber(lesson.start);
+    const lessonEndTime = parseTimeToNumber(lesson.end);
+
+    if(optimalSchedule.length === 1) {
+      if(lessonEndTime <= lastEndTime) {
+        optimalSchedule[0] = lesson;
+        lastEndTime = lessonEndTime;
+        continue;
+      }
+    }
+
+    if(lessonStartTime >= lastEndTime) {
+      optimalSchedule.push(lesson);
+      lastEndTime = lessonEndTime;
+    }
+  }
+
+  return optimalSchedule;
+}
+
+
+// console.log(getOptimalRoomSchedule(roomScheduleOptions));
--- typescript/algorithms/index.ts
+++ typescript/algorithms/index.ts
@@ -2,3 +2,5 @@ export * as arraySearchAlgorithms from './array/search';
 export * as arraySortAlgorithms from './array/sort';
 
 export * as graphSearchAlgorithms from './graph/search';
+
+export * as asd from './greedyAlgorithms'
